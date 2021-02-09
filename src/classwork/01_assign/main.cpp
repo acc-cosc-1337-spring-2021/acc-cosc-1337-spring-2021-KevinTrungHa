e4c8146306ec18004897d81623d0c4f9a7a4c975
KevinTrungHa <trung.ha@g.austincc.edu>
2021-02-09 17:23:31 -0600

    homework 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -2,11 +2,33 @@
 
 //Write include for capturing input from keyboard and displaying output to screen
 
+#include <iostream>
+#include "types.h"
+#include "types.cpp"
 
+using std::cin;
+using std::cout;
 
-int main() 
+int main()
+
 {
 
+	int num;
+	int num1 = 4;
+	int result;
+
+	cout << "Please input an integer number: ";
+	cin >> num;
+
+	result = multiply_numbers(num);
+
+	cout << "Here is the result: " << result;
+
+	result = multiply_numbers(num1);
+
+	cout<< "Here is the result of the new value: " << result;
+
 	return 0;
+
 }
 
--- src/classwork/01_assign/types.h
+++ src/classwork/01_assign/types.h
@@ -1,5 +1,27 @@
 //DON'T ERASE THIS 
-int sum_numbers(int num1, int num2);
+//int sum_numbers(int num1, int num2);
+
+
+#include <iostream>
+using namespace std;
+
+//#include "main.cpp"
+//#include "types.cpp"
+
+//create an int value-function prototype named multiply_numbers 
+//with one int parameter named num1
+
+int multiply_numbers(int num1);
+
+int main()
+{
+    int num1;
+    cout << "Please enter a number:";
+    cin >> num1;
+    int result = multiply_numbers(num1);
+    cout<<"Result:"<<result;
+    return 0;
+}
 
 
 

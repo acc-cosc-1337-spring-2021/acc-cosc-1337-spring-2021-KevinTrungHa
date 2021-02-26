7f5deeca6a899134224125ac3fe95931f982c0c1
KevinTrungHa <trung.ha@g.austincc.edu>
2021-02-25 19:18:09 -0600

    Homework 3

--- src/classwork/03_assign/decision.h
+++ src/classwork/03_assign/decision.h
@@ -2,5 +2,7 @@
 #include <string>
 using std::string;
 
+
+//write function prototypes for get_letter_grade_using_if and get_letter_grade_using_switch
 string get_letter_grade_using_if (int grade);
 string get_letter_grade_using_switch( int grade);
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -10,8 +10,8 @@ using std::cout;    using std::cin;
 
 int main() 
 {	
-	
-	// get input from user
+
+	// get input from user, capture a number from keyboard
 	int grade_from_user;
 	cout << "Please input in a numeric grade between 0 and 100: ";
 	cin >> grade_from_user;
@@ -20,6 +20,8 @@ int main()
 	// verify if input that user entered stays between range 0 to 100
 	if (grade_from_user > 0 && grade_from_user < 100)
 	{
+		//call functions get_letter_grade_using_if and get_letter_grade_using_switch 
+		//to display the letter grade to screen
 		cout << "Using if, your letter grade would be: " << get_letter_grade_using_if(grade_from_user) << "\n";
 		cout << "Using switch, your letter grade would be: " << get_letter_grade_using_switch(grade_from_user) << "\n";
 	}

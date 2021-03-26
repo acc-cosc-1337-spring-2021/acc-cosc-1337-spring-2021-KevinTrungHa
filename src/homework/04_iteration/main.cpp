eb771313469915a44b87e68dd4c9db9d4a5586f2
KevinTrungHa <trung.ha@g.austincc.edu>
2021-03-26 17:33:38 -0500

    Homework 5

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -19,12 +19,12 @@ int main()
 	{
 		int num_choice;
 		string dna;
-		cout<<"Enter 1 for Get GC Content, or 2 for Get DNA Complement: ";
+		cout<<"Please enter 1 for Get GC Content, or 2 for Get DNA Complement: ";
 		cin>>num_choice;
 		
 		if(num_choice == 1)
 		{	
-			cout<<"\nEnter the DNA string: ";
+			cout<<"\nPlease enter the DNA string: ";
 			cin>>dna;
 			double gc_content = get_gc_content(dna);
 			cout<<"\nThe GC content is: "<<gc_content;
@@ -32,13 +32,13 @@ int main()
 
 		else
 		{
-			cout<<"\nEnter the DNA string: ";
+			cout<<"\nPlease enter the DNA string: ";
 			cin>>dna;
 			string complement = get_dna_complement(dna);
 			cout<<"\nThe DNA complement is: "<<complement;
 		}
 
-		cout<<"\nEnter y to continue: ";
+		cout<<"\nPlease enter y to continue: ";
 		cin>>choice;
 	} while(choice == 'y' || choice == 'Y');
 	

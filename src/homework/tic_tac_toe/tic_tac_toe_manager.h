9a7a9e811e9e9e4f58c80600470339f4054c40c0
KevinTrungHa <trung.ha@g.austincc.edu>
2021-04-16 17:55:35 -0500

    Homework 8

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,21 @@
 //h
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+#include "tic_tac_toe.h"
+
+class TicTacToeManager
+{
+friend std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager);
+
+public:
+    void get_winner_totals(int& o, int& x, int& t);
+    void save_game(TicTacToe game);
+
+private:
+    void update_winner_count(std::string winner);
+    vector<TicTacToe> games;
+    int o_wins{0}, x_wins{0}, ties{0};
+};
+
+
+#endif
